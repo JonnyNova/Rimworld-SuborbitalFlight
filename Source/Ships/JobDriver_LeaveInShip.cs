4c07ff20e4c44ce7da9e3db24c10e64a769c98b9
JonnyNova <JonnyNova@users.noreply.github.com>
2018-11-24 14:23:51 -0500

    Fix unimplemented TryMakePreToilReservations in JobDriver_LeaveInShip

--- Source/Ships/JobDriver_LeaveInShip.cs
+++ Source/Ships/JobDriver_LeaveInShip.cs
@@ -55,7 +55,7 @@ namespace OHUShips
 
         public override bool TryMakePreToilReservations(bool errorOnFailed)
         {
-            throw new NotImplementedException();
+            return true;
         }
     }
 }
